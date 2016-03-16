b369bb6d2e835bd06f5556b6c18392039385cab9
rolpe <ronlipkin14@gmail.com>
2016-03-16 14:10:04 -0400

    Initial Commit

---
+++ myMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myMonster
+//
+//  Created by Ron Lipkin on 3/16/16.
+//  Copyright © 2016 rolp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
